43ad8dd1d3279130032d36eb403676afd7368e41
YQ <916378508@qq.com>
2022-03-19 13:44:55 +0800

    [Debug] enable StrictMode

--- android/app/src/main/kotlin/com/yqq/cloudmusic/cloud_music/MainActivity.kt
+++ android/app/src/main/kotlin/com/yqq/cloudmusic/cloud_music/MainActivity.kt
@@ -1,6 +1,30 @@
 package com.yqq.cloudmusic.cloud_music
 
+import android.os.Bundle
+import android.os.Environment
+import android.os.StrictMode
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableStrictMode()
+    }
+
+    private fun enableStrictMode() {
+        // 分别为MainThread和VM设置Strict Mode
+        if (BuildConfig.DEBUG) {
+            Log.d(this.javaClass.name, "enable strict mode")
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build())
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyDeath()
+                    .build())
+        }
+    }
 }
---
+++ android/app/src/main/kotlin/com/yqq/cloudmusic/cloud_music/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.yqq.cloudmusic.cloud_music
+
+import android.os.StrictMode
+import android.util.Log
+import io.flutter.app.FlutterApplication
+
+class MyApplication : FlutterApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
